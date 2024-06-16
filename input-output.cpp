55dc9f7d418ff70073c650afdc9671c16da8ffa7
MohammadFahad1 <fahad4bangladesh@gmail.com>
2024-06-16 11:17:14 +0600

    Initial Commit

---
+++ input-output.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    long long int b;
+    // cin >> a;
+    // cout << a;
+    // cin >> a >> b;
+    // cout << a << endl
+    //      << "Hello World" << endl
+    //      << b << endl;
+
+    // Typecasting
+    return 0;
+}
