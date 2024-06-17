9df52963032092bc00e1c8a287dfd2d518cc6064
MohammadFahad1 <fahad4bangladesh@gmail.com>
2024-06-17 13:07:06 +0600

    Declaring and using dynamic variable and functions using c++

--- dynamic_variable.cpp
+++ dynamic_variable.cpp
@@ -2,11 +2,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int *fun()
+{
+    int *a = new int;
+    *a = 100;
+    int **p = &a;
+    cout << "Address of a: " << p << endl;
+    return a;
+}
+
 int main()
 {
+    // Regular Variable (Static Variable)
     // int x = 10;
+
+    // Dynamic Variable
     int *a = new int;
     *a = 10;
     cout << *a << endl;
+
+    float *f = new float;
+    *f = 20.50;
+    cout << fixed << setprecision(2) << *f << endl;
+
+    // Dynamic Function
+    int *p = fun();
+    cout << p << endl;
+
     return 0;
 }
