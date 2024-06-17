740eaf6cba9195072e67a7ce9ad04dbda5fd9bc8
MohammadFahad1 <fahad4bangladesh@gmail.com>
2024-06-17 12:45:27 +0600

    Creating a Dynamic Variable and using pointer

---
+++ dynamic_variable.cpp
@@ -0,0 +1,12 @@
+// #include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // int x = 10;
+    int *a = new int;
+    *a = 10;
+    cout << *a << endl;
+    return 0;
+}
