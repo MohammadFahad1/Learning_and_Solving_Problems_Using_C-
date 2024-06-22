ed8cf8743c13e6c24f5c95d9cc57b7b8bc1dd507
MohammadFahad1 <fahad4bangladesh@gmail.com>
2024-06-22 12:37:50 +0600

    String Modifier Methods in C++

--- string_modifier_methods.cpp
+++ string_modifier_methods.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main()
 {
-    string a = "Hello";
-    string b = "World";
+    // string a = "Hello";
+    // string b = "World";
     /*
     // Concating String
         a = a + b;
@@ -13,14 +13,15 @@ int main()
         cout << a << endl
              << b << endl;
     */
-
-    // Adding a single character after the string
-    // a = "HelloA";
-    // a += "A";
-    a.push_back('A');
-    cout << a << endl;
-    a.pop_back();
-    cout << a << endl;
+    /*
+        // Adding a single character after the string
+        // a = "HelloA";
+        // a += "A";
+        a.push_back('A');
+        cout << a << endl;
+        a.pop_back();
+        cout << a << endl;
+     */
 
     /*
     // Other Available Methods:
@@ -30,6 +31,25 @@ int main()
         s.replace() -> replace a portion of the string.
         s.insert() -> insert a portion to a specific position.
      */
+
+    // a = "Gelo";
+    // a.assign("Gelo");
+    // cout << a << endl;
+
+    string s = "Hello World";
+    // s.erase(5, s.size() - 6);
+    // s.erase(5, 3);
+    // cout << s << endl;
+    /*
+        // Replacing blank space with Underscore
+        s.replace(5, 1, "_");
+        s.replace(5, 0, "oo");
+        cout << s << endl;
+     */
+
+    // s.replace(5, 0, " Fahad");
+    s.insert(5, " Fahad");
+    cout << s << endl;
 
     return 0;
 }
