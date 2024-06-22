82f93a58cdc6904fa6ab5670b0567db889868128
MohammadFahad1 <fahad4bangladesh@gmail.com>
2024-06-22 12:00:06 +0600

    C++ String Element Access Methods

---
+++ string_element_access.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    /*
+        // Read Character by Index
+        cout << s[0] << endl;
+        cout << s.at(0) << endl;
+    */
+
+    /*
+        // Getting First and Last character of the string
+        cout << s.[0] << endl;
+        cout << s.front() << endl;
+        cout << s[s.size() - 1] << endl;
+        cout << s.back() << endl;
+    */
+
+       return 0;
+}
