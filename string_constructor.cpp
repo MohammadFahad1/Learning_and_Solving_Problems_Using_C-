401e7dfac64112ccc3d210c611d814a2f369fede
MohammadFahad1 <fahad4bangladesh@gmail.com>
2024-06-25 21:23:36 +0600

    Using String Constructor to perform different operations

---
+++ string_constructor.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // 1.
+    // string s = "Hello World";
+    // 2.
+    // string s("Hello World");
+    // 3.
+    // string s("Hello World", 7);
+    // 4.
+    // string a = "Hello World";
+    // string s(a, 4);
+    // 5.
+    string s(25, 'A');
+    cout << s << endl;
+    return 0;
+}
