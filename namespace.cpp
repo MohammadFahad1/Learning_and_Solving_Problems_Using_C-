c35b9acb468b675d0ea4fade954dd2b51670485e
MohammadFahad1 <fahad4bangladesh@gmail.com>
2024-06-28 12:39:34 +0600

    Namespace using cpp

--- namespace.cpp
+++ namespace.cpp
@@ -6,11 +6,32 @@ namespace Rakib
     int age = 24;
     void hello()
     {
+        cout << "Rakib namespace" << endl;
+    }
+
+    // We can also have classes inside namespace
+    // class Person{
+
+    // }
+}
+
+namespace Sakib
+{
+    int age2 = 30;
+    void hello2()
+    {
+        cout << "Sakib namespace" << endl;
     }
 }
 
+using namespace Rakib;
+using namespace Sakib;
+
 int main()
 {
-
+    cout << age << endl;
+    cout << age2 << endl;
+    hello();
+    hello2();
     return 0;
 }
